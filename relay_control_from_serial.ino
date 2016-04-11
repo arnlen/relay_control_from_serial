2d8aa970fdf2e2753183655da32660f81d1121da
Arnaud Lenglet <arlenglet@gmail.com>
2016-04-11 08:36:58 +0200

    Initial commit

---
+++ relay_control_from_serial.ino
@@ -0,0 +1,28 @@
+/*
+  Arnlen
+  Control a relay state from serial commands.
+ */
+
+const int RELAY = 7; // relay on pin 7
+boolean relayState = 0; // relay state (on / off)
+
+void setup() {
+  pinMode(RELAY, OUTPUT);
+
+  Serial.begin(9600);
+  while (!Serial) { }
+}
+
+void loop() {
+  int message = Serial.read()-'0';
+
+  if (message != -49) {
+    Serial.println(message);
+
+    if (message == 1 || message == 0) {
+      relayState = message;
+      digitalWrite(RELAY, relayState); 
+    }
+  }
+  delay(1000); // delay in between reads for stability
+}
