680f17a67f8cc30d99063131a14aede5d3e40ef8
Arnaud Lenglet <arlenglet@gmail.com>
2016-04-17 17:38:02 +0200

    doc(comments): update comments for better understanding

--- relay_control_from_serial.ino
+++ relay_control_from_serial.ino
@@ -3,26 +3,28 @@
   Control a relay state from serial commands.
  */
 
-const int RELAY = 7; // relay on pin 7
-boolean relayState = 0; // relay state (on / off)
+const int RELAY = 7;       // relay on pin 7  
+boolean relayState = 0;    // relay state (on / off)
 
-void setup() {
+void setup() {  
   pinMode(RELAY, OUTPUT);
 
-  Serial.begin(9600);
-  while (!Serial) { }
+  Serial.begin(9600);      // Must match the Rails SERIAL_BAUD_RATE config
+  while (!Serial) { }      // Wait for serial communication to be ready
 }
 
-void loop() {
-  int message = Serial.read()-'0';
+void loop() {  
+  int message = Serial.read()-'0'; // Remove "0" which is the -48 value
+
+  Serial.println(message);         // Output on serial for debug purpose
 
   if (message != -49) {
-    Serial.println(message);
 
     if (message == 1 || message == 0) {
       relayState = message;
-      digitalWrite(RELAY, relayState);
+      digitalWrite(RELAY, relayState); // Write to relay
     }
   }
-  delay(1000); // delay in between reads for stability
+
+  delay(1000); // wait for 1 second before every loop
 }
