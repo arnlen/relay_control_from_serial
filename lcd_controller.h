1c73d4d4c3828580a2ae7640df97d7ac4f93bdcc
Arnaud Lenglet <arlenglet@gmail.com>
2016-04-25 19:39:22 +0200

    feat(lcd): add LCD controller with #setup and #display methods

---
+++ lcd_controller.h
@@ -0,0 +1,27 @@
+/*
+ *
+ * Arnlen
+ * Controle the LCD screen
+ * Allow display of serial data
+ *
+ */
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup_lcd() {
+  lcd.begin(16, 2);
+}
+
+void display_serial_data() {
+  if (Serial.available()) {
+    delay(100);
+
+    lcd.clear();
+
+    while (Serial.available() > 0) {
+      lcd.write(Serial.read());
+    }
+  }
+}
